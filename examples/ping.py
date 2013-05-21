81e1423f6beedca3d737edf4f1600ceea8f73059
Michele Orrù <maker@python.it>
2013-05-21 19:48:22 +0200

    Test example ping.py.
    New file ping.py which allows to test the scheduler via simple command line and
    output checking.

---
+++ examples/ping.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python -u
+from time import sleep, time
+from sys import argv
+
+ping = argv[1] if len(argv) > 1 else 'ping %d' % time()
+delay = int(argv[2]) if len(argv) > 2 else 1
+
+while True:
+    print ping
+    sleep(delay)
